182f74a793ed1fa091e1e911b6b48566ecedb57e
Eduardo-Suarez <draco_008@hotmail.com>
2023-03-24 16:24:43 -0700

    integration of the design of the cards and visualization of the images

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getGifs } from '../helpers/getgifs'
+import { GifItem } from './GifItem';
 
 export const GifGrid = ( {category} ) => {
 
+  const [images, setImages] = useState ([]);
+
+  const getImages = async() => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
    useEffect( () => {
-        getGifs(category);
+        getImages();
    }, []);
 
   return (
     <>
         <h3 >{category}</h3>
+
+        <div className='card-grid'>
+          {
+            images.map(image => (
+              <GifItem 
+                key={image.id}
+                //distribuye todas las propiedades de la imagen, asi es mas facil hacer la destructuracion.
+                {...image}/>
+            ))
+          }
+        </div>
         
     </>
   )
